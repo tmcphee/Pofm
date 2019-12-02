ddbd85fd769d64dad5a2f3590a9e74669a7e4078
FapfapDragon <48346054+FapfapDragon@users.noreply.github.com>
2019-12-02 17:24:14 -0500

    Update CreateFile.h

--- Pofn/CreateFile.h
+++ Pofn/CreateFile.h
@@ -3,18 +3,18 @@
 int createFileLocal(char *sourcec, char *namec)
 {
 	DIR *source_folder; //where the file should be created
-	char *fpath, *buff, *foldpath;
-	char ch;
+	char *fpath, *buff, *foldpath; //char pointers to store locations
+	char ch;    //temp variable, just used for assigning input to file
 	String filename = createString();
 	String filepath = createString();
 	String userinput = createString();
-	FILE *userfile;
+	FILE *userfile;    //file to be created
 
-	scanString(filepath, sourcec);
-	foldpath = putString(filepath);
-	source_folder = opendir(foldpath);
+	scanString(filepath, sourcec);      //store source folder
+	foldpath = putString(filepath);     //store source folder
+	source_folder = opendir(foldpath);  //open source directory
 
-	if (source_folder == NULL)
+	if (source_folder == NULL)  //if folder does not exist return error
 	{
 		printf("Unable to open Directory %s\n", foldpath);
 		perror("The following error occured: ");
@@ -25,16 +25,18 @@ int createFileLocal(char *sourcec, char *namec)
 	free(foldpath);
 
 	closedir(source_folder);
-	scanString(filename, namec);
+	scanString(filename, namec); // load name of file
 
-#ifdef _WIN32
-	addChar(filepath, (char)(92));
+#ifdef _WIN32    //If on windows add forward slash to path otherwise add backslash, need for cross platform
+	addChar(filepath, (char)(92)); 
 #else
 	addChar(filepath, (char)(47));
 #endif
-
+        //store full file path including filename
 	stringCat(filepath, filename);
 	fpath = putString(filepath);
+        
+        //Make sure filename has a txt extension
 	char txt[5] = "txt.";
 	for (int i = 0; i < 4; i++)
 	{
@@ -43,6 +45,8 @@ int createFileLocal(char *sourcec, char *namec)
 			return 2;
 		};
 	}
+      
+        //open file and write user input to file
 	userfile = OpenFileLocal(fpath, "w");
 	printf("Continue to enter text until you are done, hit enter to submit text: \n");
 	stringGets(userinput);
@@ -57,6 +61,8 @@ int createFileLocal(char *sourcec, char *namec)
 		ch = buff[i];
 		fputc(ch, userfile);
 	}
+   
+        //close file and free memory
 	fclose(userfile);
 	free(fpath);
 	free(buff);
